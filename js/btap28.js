46eca50a92b545c23d896e1c9439f708446f3ac0
trongtran020504 <trongtran020504@gmail.com>
2025-11-21 09:36:20 +0700

    Add files via upload

---
+++ js/btap28.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('Portfolio của Trần Bình Trọng đã load xong!');
+
+    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
+
+});
